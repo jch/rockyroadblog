ca5bf040543dd50fad25889ff1df1754fcad35ba
Jerry Cheung <jch@whatcodecraves.com>
2012-01-12 23:05:57 -0500

    move js to bottom

--- footer.php
+++ footer.php
@@ -7,7 +7,25 @@
   </div><!-- wrapper -->
   <!-- BEGIN SOCIAL -->
   <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+  <script type='text/javascript'>
+    var addthis_config = {
+      ui_use_css: false
+    };
+  </script>
   <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4efff341783ccb2b"></script>
   <!-- END SOCIAL -->
+
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-25073623-1']);
+    _gaq.push(['_setDomainName', 'rockyroadblog.com']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
   </body>
 </html>
--- meta.php
+++ meta.php
@@ -4,19 +4,12 @@
   </div>
   <div class='social'>
     <!-- AddThis Button BEGIN -->
-    <script type='text/javascript'>
-    var addthis_config = {
-      ui_use_css: false
-    };
-    </script>
     <div class="addthis_toolbox addthis_default_style ">
       <a class="addthis_button_tweet"></a>
       <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
       <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
       <!-- <a class="addthis_counter addthis_pill_style"></a> -->
     </div>
-    <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4efff341783ccb2b">
-    </script>
     <!-- AddThis Button END -->
   </div>
   <div class='comments'>
