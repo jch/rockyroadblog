4c55fb9b3401869f683999f248f4c467a8fc5f01
Jerry Cheung <jch@whatcodecraves.com>
2012-01-16 01:15:16 -0500

    tweak pagination text, text shadow

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <?php get_template_part('content', get_post_format()); ?>
 <?php endwhile; ?>
 <div id="pagination">
-  <?php previous_posts_link(); ?>
-  <?php next_posts_link(); ?>
+  <?php previous_posts_link('« Newer'); ?>
+  <?php next_posts_link('Older »'); ?>
 </div>
 
 <?php get_footer(); ?>
