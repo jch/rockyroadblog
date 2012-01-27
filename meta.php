f599572f617462dacbfdc0d2b455e235a3024df5
Jerry Cheung <jch@whatcodecraves.com>
2012-01-27 14:30:47 -0800

    comment out social partial

--- meta.php
+++ meta.php
@@ -2,17 +2,14 @@
   <div class='author'>
     <p class='byline'>Posted by <?php the_author(); ?> on <?php the_date() ?></p>
   </div>
-  <div class='social'>
-    <!-- AddThis Button BEGIN -->
+  <!--<div class='social'>
     <div class="addthis_toolbox addthis_default_style ">
       <a class="addthis_button_tweet"></a>
       <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
       <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-      <!-- <a class="addthis_counter addthis_pill_style"></a> -->
     </div>
-    <!-- AddThis Button END -->
   </div>
   <div class='comments'>
     
-  </div>
+  </div>-->
 </div>
