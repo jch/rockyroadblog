ad0c05a0daebde1bb1f7d6c11a291c931e341c27
Jerry Cheung <jch@whatcodecraves.com>
2012-11-25 07:30:35 -0800

    link up sharing urls
    * float share buttons to the right
    * remove right margins for sharing sections

--- meta.php
+++ meta.php
@@ -3,18 +3,21 @@
     <p class='byline'>Posted by <?php the_author(); ?> on <?php the_date() ?></p>
   </div>
   <div class="meta-section sharing-strip">
+    <!-- <a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
+    <div class="fb-like" data-href="http://foo.com/#foosdf3d" data-send="false" data-width="450" data-show-faces="false" data-layout="button_count"></div> -->
+    
     <span class="share-btn facebook">
-      <a href="#">
+      <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink() ?>&t=<?php the_title() ?>" target="_blank">
         <span class="icon"><img src="/wp-content/themes/rockyroad/images/sharing/sharing-strip-facebook.png" alt=""></span>
-        <span class="label">Like</span>
+        <span class="label">Share</span>
       </a>
     </span>
 
     <span class="share-btn twitter">
-      <a href="#">
+      <a href="https://twitter.com/share?via=rockyroadblog&text=<?php the_title() ?>&url=<?php the_permalink() ?>" target="_blank">
         <span class="icon"><img src="/wp-content/themes/rockyroad/images/sharing/sharing-strip-twitter.png" alt=""></span>
         <span class="label">Tweet</span>
       </a>
-    </span>    
+    </span>
   </div>
 </div>
