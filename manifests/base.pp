924df3bb334d0d846b6a0c7a363ce196bb26b973
Jerry Cheung <jch@whatcodecraves.com>
2012-12-23 20:37:43 -0800

    add wordpress code configuration
    checks out wordpress versions to /opt/wordpress-$version

--- manifests/base.pp
+++ manifests/base.pp
@@ -13,6 +13,38 @@ file { '/etc/motd':
 #   ensure   => present,
 #   www_root => '/var/www/rockyroadblog.com',
 # }
+#### Wordpress configuration
+
+# Installs Wordpress version and symlinks to it
+# $name or $path - symlink file to create
+# $version       - a valid wordpress tag. e.g. 3.5
+define wordpress::code(
+  $version,
+  $path = undef
+) {
+
+  $symlink = $path ? {
+    undef   => $name,
+    default => $path
+  }
+  $target = "/opt/wordpress-$version"
+
+  vcsrepo { $target:
+    ensure     => present,
+    provider   => svn,
+    source     => "http://core.svn.wordpress.org/tags/$version"
+  }
+
+  file { $symlink:
+    target => $target
+  }
+}
+
+$site_path = "/var/www/$fqdn"
+
+wordpress::code { $site_path:
+  version => '3.5'
+}
 
 include wordpress
 
